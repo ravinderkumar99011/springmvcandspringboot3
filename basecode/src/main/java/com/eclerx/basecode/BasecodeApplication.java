f92034c267132ed224870b40ba667284a65ff0e3
ravinder077 <ravinder077@gmail.com>
2024-04-25 11:30:35 +0530

    first commit

---
+++ basecode/src/main/java/com/eclerx/basecode/BasecodeApplication.java
@@ -0,0 +1,15 @@
+package com.eclerx.basecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class BasecodeApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecodeApplication.class, args);
+	}
+
+}
